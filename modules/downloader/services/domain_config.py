48d4692294950ef169dd8aefe8607312d6d1d0cc
Developer <dev@local>
2026-03-18 04:01:10 +0000

    Add: domain_config.py with supported domains and Spotify URL checker

---
+++ modules/downloader/services/domain_config.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from urllib.parse import urlparse
+
+
+SUPPORTED_DOMAINS = (
+    "youtube.com",
+    "youtu.be",
+    "youtube-nocookie.com",
+    "spotify.com",
+    "bilibili.com",
+    "bilibili.tv",
+    "tiktok.com",
+    "instagram.com",
+    "twitter.com",
+    "x.com",
+    "facebook.com",
+    "reddit.com",
+    "vk.com",
+    "soundcloud.com",
+    "vimeo.com",
+    "twitch.tv",
+    "dailymotion.com",
+)
+
+
+def is_spotify_url(url: str) -> bool:
+    try:
+        parsed = urlparse(url)
+        return "spotify.com" in parsed.netloc.lower()
+    except Exception:
+        return False
